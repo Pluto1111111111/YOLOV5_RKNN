bce389b544994b544e5ba3f1d1d7f14efd81e9f2
pluto zang <1787215088@qq.com>
2024-05-26 19:33:59 +0800

    批量重命名

--- tool/rename.py
+++ tool/rename.py
@@ -3,17 +3,13 @@ import os
 
 def file_rename(input_dir, output_dir):
     """
-    批量处理目录下的所有YOLO标签文件，左右翻转其bounding box坐标。
-
-    :param input_dir: str, 包含YOLO标签文件的输入目录路径。
-    :param output_dir: str, 输出翻转后标签文件的目录路径。
-    :param image_width: int, 图像的宽度，用于计算翻转后的坐标。
+    批量重命名目录下的所有文件
     """
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)  # 确保输出目录存在
 
     for filename in os.listdir(input_dir):
-        if filename.endswith('.txt'):  # 确保只处理.txt文件
+        if filename.endswith('.txt'):  # 确保只处理.txt文件   可以改为其他文件
             input_file_path = os.path.join(input_dir, filename)
             output_file_path = os.path.join(output_dir, 'gray_' + filename)
             # 重命名文件
@@ -22,7 +18,7 @@ def file_rename(input_dir, output_dir):
             print(f'{filename} -> {new_name}')
 
 # 使用示例
-input_directory = './name'  # 包含YOLO标签文件的目录
-output_directory = './new_name'  # 输出翻转后标签文件的目录
+input_directory = './name'  # 原文件路径
+output_directory = './new_name'  # 重命名路径
 
 file_rename(input_directory, output_directory)
