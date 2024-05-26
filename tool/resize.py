11f10e33ab42a083da01d06eb37920c9b34a1afc
pluto zang <1787215088@qq.com>
2024-05-26 19:34:10 +0800

    批量resize

--- tool/resize.py
+++ tool/resize.py
@@ -3,11 +3,11 @@ import cv2
 
 # 定义图片目录
 img_dir = './yuan'
-gray_dir = './resize'
+resize_dir = './resize'
 
 # 如果目标目录不存在，则创建
-if not os.path.exists(gray_dir):
-    os.makedirs(gray_dir)
+if not os.path.exists(resize_dir):
+    os.makedirs(resize_dir)
 
 # 遍历目录下的所有文件
 for filename in os.listdir(img_dir):
@@ -16,12 +16,10 @@ for filename in os.listdir(img_dir):
         # 构建完整的图片路径
         img_path = os.path.join(img_dir, filename)
         
-        # 读取图像并转换为灰度
         img = cv2.imread(img_path)
-        
         # 调整图像尺寸
         resized_img = cv2.resize(img, (640, 480))
         
         # 构建保存的路径并保存图像到gray_dir目录
-        save_path = os.path.join(gray_dir, filename)
+        save_path = os.path.join(resize_dir, filename)
         cv2.imwrite(save_path, resized_img)
